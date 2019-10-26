8df14fadae58de4d86870778f978ae7014f3477b
Yining Wang <yining.wang@utexas.edu>
2019-10-26 03:42:02 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,10 +7,12 @@ require 'directors_database'
 
 def directors_totals(nds)
   result = {}
-  nil
+  directors_database.each{|director| result[director[:name]] = gross_for_director(director)}
+  result
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  director_data[:movies].reduce(0){|sum, movie| sum + movie[:worldwide_gross]}
 end
